e0bb3de344ff60c5fc71d2c80dcc4cdd2b878f1a
charlie890414 <charlie890414@gmail.com>
2017-07-17 16:28:18 +0800

    zombie move

--- Assets/ZombieFunction.cs
+++ Assets/ZombieFunction.cs
@@ -3,16 +3,40 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ZombieFunction : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
+ 
+    // Use this for initialization
+    void Start () {
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        var player = GameObject.Find("Player");
+        Debug.Log(player.transform.position);
+        if(Vector3.Distance(gameObject.transform.position, player.transform.position) < 6){
+            if (gameObject.transform.position.y < player.transform.position.y) gameObject.transform.position += new Vector3(0, 0.01f, 0);
+            else if (gameObject.transform.position.y > player.transform.position.y) gameObject.transform.position += new Vector3(0, -0.01f, 0);
+            if (gameObject.transform.position.x < player.transform.position.x) gameObject.transform.position += new Vector3(0.01f, 0, 0);
+            else if (gameObject.transform.position.x > player.transform.position.x) gameObject.transform.position += new Vector3(-0.01f, 0, 0);
+        }
+        else{
+            int num = Random.Range(1, 4);
+            switch (num){
+                case 1:
+                    gameObject.transform.position += new Vector3(0, 0.01f, 0);
+                    break;
+                case 2:
+                    gameObject.transform.position += new Vector3(0, -0.01f, 0);
+                    break;
+                case 3:
+                    gameObject.transform.position += new Vector3(0.01f, 0 , 0);
+                    break;
+                case 4:
+                    gameObject.transform.position += new Vector3(-0.01f, 0, 0);
+                    break;
+            }
+             
+        }
+    }
 
     private void OnTriggerEnter2D(Collider2D col)
     {
